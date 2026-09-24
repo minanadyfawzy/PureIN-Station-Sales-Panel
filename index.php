0a890da07060aef5b882b56afe0e14e52ea69c52
minanadyfawzy <98706088+minanadyfawzy@users.noreply.github.com>
2026-09-24 20:41:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -41,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>لوحة الوقود</title>
+  <title>لوحة الدخول</title>
   <link rel="stylesheet" href="style.css">
 </head>
 
